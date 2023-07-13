dfe7723806c5543523ba6223e1a86e8b8dcf5cb4
Your Name <you@example.com>
2023-07-13 11:58:36 -0700

    adv

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - print line to standard error
+* Return: 1
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
