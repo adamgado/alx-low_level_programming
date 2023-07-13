1683b9eba6a3bf1a20ac1d91cf62338a6a7a8ce8
Your Name <you@example.com>
2023-07-13 11:28:43 -0700

    mandatory

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - print line using printf followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
