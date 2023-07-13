34feb9130c94cea7832fe8183d9e6e11ca339b60
Your Name <you@example.com>
2023-07-13 12:01:25 -0700

    adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - print line to standard error
 * Return: 1
 */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
