0aecaaa9e74e6bfdbd953e99a91547c04e9dba8d
adamgado <adamgado33@gmail.com>
2023-10-26 00:45:56 -0700

    0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - print binary of decimal
+ * @n: number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	int a, count = 0;
+	unsigned long int current_bit;
+
+	for (a = 63; a >= 0; a--)
+	{
+		current_bit = n >> a;
+
+		if (current_bit & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
