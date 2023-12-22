6433db909fbf88b0bb639f4e0effd036dea7bce5
adamgado <adamgado33@gmail.com>
2023-12-22 03:09:54 -0800

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+  * hash_table_get - get value of key in hash table
+  * @ht: hash table 
+  * @key: key
+  * Return: value of the key
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int x = 0;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	x = key_index((unsigned char *) key, ht->size);
+	current = ht->array[x];
+
+	if (current == NULL)
+		return (NULL);
+
+	while (strcmp(key, current->key) != 0)
+		current = current->next;
+
+	return (current->value);
+}
