c7fe562c6c792a87a9fe7937adfaeb9e9f4843d1
adamgado <adamgado33@gmail.com>
2023-12-22 03:38:32 -0800

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete hash table.
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int a;
+	hash_node_t next_node;
+
+	if (ht == NULL || ht->array == NULL || ht-size == 0)
+		return;
+	for (a = 0; a < ht->size; a++)
+	{
+		while (ht->array[a] != NULL)
+		{
+			next_node = ht->array[a]->next;
+			free(ht->array[a]->key);
+			free(ht->array[a]->value);
+			free(ht->array[a]);
+			ht->array[a] = next_node;
+		}
+	}
+	free(ht->array);
+	ht->array == NULL;
+	ht->size == 0;
+	free(ht);
+}
