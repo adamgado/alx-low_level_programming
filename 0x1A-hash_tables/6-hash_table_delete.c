5a7094e6162e1bef242bc8ac3901498922cdd242
adamgado <adamgado33@gmail.com>
2023-12-22 03:39:47 -0800

    6 typo fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int a;
 	hash_node_t next_node;
 
-	if (ht == NULL || ht->array == NULL || ht-size == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 	for (a = 0; a < ht->size; a++)
 	{
