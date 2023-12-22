244c0d410f18a9b6f1f336f699352ae624a0a741
adamgado <adamgado33@gmail.com>
2023-12-22 02:25:59 -0800

    0 try 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int a;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
-	{
+	if (size == 0)
 		return (NULL);
-	}
+
+	new_table = calloc(1, sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_node_t *));
+	new_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
-	}
-	for (a = 0; a < size; a++)
-	{
-		new_table->array[a] = NULL;
 	}
 	return (new_table);
 }
